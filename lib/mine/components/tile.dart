18d9128b1b8e1cca3e213fec2b33cc074a8af975
HydreigonGuy <ad.thibault18@gmail.com>
2025-02-22 10:41:51 +0100

    clicking on a tile now reveals if it's a bomb or not

--- lib/mine/components/tile.dart
+++ lib/mine/components/tile.dart
@@ -1,21 +1,31 @@
 import 'package:flame/components.dart';
 import 'package:flame/collisions.dart';
+import 'package:flame/sprite.dart';
 
 import 'package:my_app/mine/mine_game.dart';
 
-class Tile extends SpriteComponent with Tappable, HasGameRef<MineGame> {
+class Tile extends SpriteAnimationComponent with Tappable, HasGameRef<MineGame> {
   bool clicked = false;
   bool isBomb = false;
+  late final SpriteSheet spriteSheet;
   
   @override
   Future<void>? onLoad() async {
-    sprite = await gameRef.loadSprite('mine_default_tile.png');
+    spriteSheet = SpriteSheet(
+      image: await await gameRef.images.load('mine_tile.png'),
+      srcSize: Vector2(10.0, 10.0),
+    );
+    animation = spriteSheet.createAnimation(row: 0, stepTime: 1, to: 1);
     return super.onLoad();
   }
 
   @override
   bool onTapUp(_) {
     clicked = true;
+    if (isBomb)
+      animation = spriteSheet.createAnimation(row: 10, stepTime: 1, to: 1);
+    else
+      animation = spriteSheet.createAnimation(row: 1, stepTime: 1, to: 1);
     return true;
   }
 
